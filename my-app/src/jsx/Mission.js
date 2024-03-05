d3ad2483cf645c202b9b0c067a7b14fb814ab908
AdamJednacz <adamjednacz4@gmail.com>
2024-03-05 19:06:42 +0100

    mission

--- my-app/src/jsx/Mission.js
+++ my-app/src/jsx/Mission.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Mission = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="mission">
+            <div className="container">
+                <h1>Main Focus/Mission Statement</h1>
+                <div className="mission_items">
+                    <div className="mission_item">
+                        <span>1</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis
+                            placerat.</p>
+                    </div>
+                    <div className="mission_item">
+                        <span>2</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis
+                            placerat, magna mauris porttitor tortor, a auctor est felis ut nisl.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
