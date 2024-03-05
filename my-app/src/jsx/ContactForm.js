54adb3fdba5631ff6f8f68366499aaa89b20d02b
AdamJednacz <adamjednacz4@gmail.com>
2024-03-05 20:30:35 +0100

    cotact_form

--- my-app/src/jsx/ContactForm.js
+++ my-app/src/jsx/ContactForm.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import form_img from "../assets/form_img.png"
+import btn_arrow_white from "../assets/white_arrow.png";
 
 const ContactForm = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="contact_form">
+            <div className="container">
+                <h1>Contact Us</h1>
+                <div className="contact_form_items">
+                    <form>
+                        <input type="text"  placeholder="Name" />
+                        <input type="number" placeholder="Phone Number"/>
+                        <input type="email" placeholder="E-mail"/>
+                        <input type="text" placeholder="Interested In"/>
+                        <input type="text" placeholder="Message"/>
+                    </form>
+                    <img src={form_img} alt={form_img}/>
+                </div>
+                <div className="contact_form_btn">
+                    <p>Send Email</p>
+                    <img src={btn_arrow_white} alt={btn_arrow_white}/>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- my-app/src/jsx/ProjectsMain.js
+++ my-app/src/jsx/ProjectsMain.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import btn_arrow from "../assets/btn_view_arrow.svg";
+import btn_arrow_white from "../assets/white_arrow.png";
+import project1 from "../assets/projekt1.png";
+import project2 from "../assets/projekt2.png";
+import project3 from "../assets/projekt3.png";
+import project4 from "../assets/projekt4.png";
 
 const ProjectsMain = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="projects_main">
+            <div className="container">
+               <h1>Our Projects</h1>
+                <div className="projects_main_items">
+                    <div className="projects_main_item">
+                        <div className="projects_main_item_text">
+                            <h2>Sample</h2>
+                            <h2>Project</h2>
+                            <div className="projects_main_item_text_btn">
+                                <p>View More</p>
+                                <img src={btn_arrow_white} alt={btn_arrow_white}/>
+                            </div>
+                        </div>
+                        <img className="projects_main_item_text_img" src={project2} alt={project2}/>
+                    </div>
+                    <div className="projects_main_item">
+                        <img className="projects_main_item_img" src={project1} alt={project1}/>
+                        <img className="projects_main_item_img" src={project4} alt={project4}/>
+                        <img className="projects_main_item_img" src={project3} alt={project3}/>
+                    </div>
+                </div>
+                <div className="projects_main_btn">
+                    <p>All projects</p>
+                    <img src={btn_arrow_white} alt={btn_arrow_white}/>
+                </div>
+            </div>
+        </section>
     );
 };
 
