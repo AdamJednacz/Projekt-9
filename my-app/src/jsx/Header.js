6830055f9f94fc4ae46c46e985ea440d3d0f682f
AdamJednacz <adamjednacz4@gmail.com>
2024-03-17 16:58:15 +0100

    hamburger menu

--- my-app/src/jsx/Header.js
+++ my-app/src/jsx/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from "../assets/Logo.png"
 import {Link, NavLink} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars,faX} from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+        const [isOpen,setIsOpen] = useState(false)
+
+        const handleOpenMenu = () => {
+            setIsOpen(!isOpen)
+        }
+
+
     return (
         <header>
             <div className="container">
+                <div className="header_menu_hamburger">
                 <img alt={logo} src={logo}/>
-                <ul className="header_menu">
+                {isOpen ? (
+                    <FontAwesomeIcon className="header_icon"  icon={faX} onClick={handleOpenMenu} />
+                ) : (
+                    <FontAwesomeIcon className="header_icon" icon={faBars} onClick={handleOpenMenu} />
+                )}
+                </div>
+                <ul className={`header_menu ${isOpen ? 'open' : ''}`}>
                     <li className="header_menu_item"><NavLink className="link" to={"/"}>Main</NavLink></li>
                     <li className="header_menu_item"><NavLink className="link" to={"/photos"}>Gallery</NavLink></li>
                     <li className="header_menu_item"><NavLink className="link" to={"/projects"}>Projects</NavLink></li>
